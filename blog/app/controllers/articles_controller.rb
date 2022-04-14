e71b64c217c90666f44683730b3ae5a7cc7316c2
ethan <USER@acer-swift>
2022-04-14 14:54:17 +0800

    finish authentication setting

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,4 +1,9 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "ethan", password: "1234", except: [:index, :show]
+
+  def index
+    @articles = Article.all
+  end
   def index
     @articles = Article.all
   end
--- blog/app/controllers/comments_controller.rb
+++ blog/app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  http_basic_authenticate_with name: "ethan", password: "1234", except: [:index, :show]
+
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
